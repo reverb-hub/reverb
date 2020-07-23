87857682deb9db8b425e26e4bf1dbef1f95b50ad
afinch7 <andyfinch7@gmail.com>
2020-07-22 23:26:07 -0400

    resolve routes more better

--- core/route-resolver.ts
+++ core/route-resolver.ts
@@ -1,25 +1,44 @@
 import { Type } from '../decorators/module.ts';
-import { COMPONENT_TYPE, MODULE_METADATA, PATH_METADATA, ROUTE_ARGS_METADATA } from '../common/constants.ts';
+import { COMPONENT_TYPE, MODULE_METADATA, PATH_METADATA, METHOD_METADATA } from '../common/constants.ts';
 import { isConstructor, isMethod } from '../util/check.ts';
+import { HttpMethod } from '../common/http.ts';
+import { ServerRequest, Response } from '../deps.ts';
 
+interface PlaceHolderHandler {
+    (req: ServerRequest): Response
+}
+
+interface RouteMap {
+    [r: string]: Route
+}
+
+interface Route {
+    methods?: {
+        [key in HttpMethod]?: PlaceHolderHandler
+    };
+    subRoutes: RouteMap;
+}
 
 export class RouteResolver {
+    private readonly routes: RouteMap;
     constructor(private appModule: Type<any>) {
+        this.routes = this.resolveRoutes();
     }
 
-    resolveRoutes(): Map<string, [Type<any>, string]> {
+    private resolveRoutes(): RouteMap {
         if (Reflect.getMetadata(COMPONENT_TYPE.MODULE, this.appModule) !== true) {
             throw "non module supplied"
         }
 
         const controllers = Reflect.getMetadata(MODULE_METADATA.CONTROLLERS, this.appModule)
 
-        const controllerMappings = new Map<string, [Type<any>, string]>()
+        const routeMap: RouteMap = {};
 
         controllers.forEach((controller: Type<any>) => {
             if (Reflect.getMetadata(COMPONENT_TYPE.CONTROLLER, controller) !== true) {
                 throw "non controller in controllers"
             }
+            // TODO inject things here
             const controllerInstance = new controller()
             const path = Reflect.getMetadata(PATH_METADATA, controller)
             const methods = Object.getOwnPropertyNames(controller.prototype).filter((property) => {
@@ -28,11 +47,23 @@ export class RouteResolver {
 
             methods.forEach((method) => {
                 const mappingPath = Reflect.getMetadata(PATH_METADATA, controller.prototype[method])
-                const paramMetadata = Reflect.getMetadata(ROUTE_ARGS_METADATA, controller.prototype.constructor, method)
-                controllerMappings.set(path + mappingPath, [controllerInstance, method])
+                const existingRoute = routeMap[path + mappingPath];
+                const methodType: HttpMethod = Reflect.getMetadata(METHOD_METADATA, controller.prototype[method]);
+                if (existingRoute.methods && existingRoute.methods[methodType]) {
+                    // TODO expand this error message
+                    throw "Route defined twice";
+                }
+                routeMap[path + mappingPath] = {
+                    methods: {
+                        [methodType]: controllerInstance[method]
+                    },
+                    subRoutes: {
+                        ...existingRoute.subRoutes
+                    }
+                };
             })
         })
 
-        return controllerMappings
+        return routeMap
     }
 }
